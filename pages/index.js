4e08cb5c67b3fc3b5f0093991e4ea59ad52a354c
CaioVarela <89794997+CaioVarela@users.noreply.github.com>
2024-08-02 13:09:01 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luana, eu te amo. Se você tá lendo isso, dê uma risadinha!</h1>
+    return <h1>Luana, eu te amo. Se você tá lendo isso, dê uma risadinha! teste 2</h1>
 }
 
 export default Home;
